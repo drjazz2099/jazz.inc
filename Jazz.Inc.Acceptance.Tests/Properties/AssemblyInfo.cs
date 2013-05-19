2ff7b78f86a04999a0976fa02e0ec078f5373b7b
unknown <jas@NB-jas.erni2.ch>
2013-05-19 16:58:31 +0200

    test

---
+++ Jazz.Inc.Acceptance.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jazz.Inc.Acceptance.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Erni Management Services")]
+[assembly: AssemblyProduct("Jazz.Inc.Acceptance.Tests")]
+[assembly: AssemblyCopyright("Copyright © Erni Management Services 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eaad0e37-8af8-48ba-9f28-676d078dbc30")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Jazz.Inc.Acceptance.Tests/Specs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Jazz.Inc.Acceptance.Tests
+{
+    [TestFixture]
+    public class Specs
+    {
+        [Test]
+        public void Foo()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
