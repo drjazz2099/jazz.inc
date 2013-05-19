7a2d501fe8f3a791bc9b2b981fc6f11806d68a33
unknown <jas@NB-jas.erni2.ch>
2013-05-19 17:30:43 +0200

    test

---
+++ Jazz.Inc.Acceptance.Tests/BasicSpecs.feature
@@ -0,0 +1,10 @@
+﻿Feature: BasicSpecs
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Important information on the home page
+	Given I am on the internet
+	When I vist the Jazz Inc website
+	Then I should see the correct company name
---
+++ Jazz.Inc.Acceptance.Tests/BasicSpecsSteps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Jazz.Inc.Acceptance.Tests
+{
+    [Binding]
+    public class BasicSpecsSteps
+    {
+        [Given(@"I am on the internet")]
+        public void GivenIAmOnTheInternet()
+        {
+        }
+        
+        [When(@"I vist the Jazz Inc website")]
+        public void WhenIVistTheJazzIncWebsite()
+        {
+        }
+        
+        [Then(@"I should see the correct company name")]
+        public void ThenIShouldSeeTheCorrectCompanyName()
+        {
+        }
+    }
+}
