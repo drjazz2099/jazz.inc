2394ff8a95248060c7579b2ad7bad2a3a7aed22a
unknown <jas@NB-jas.erni2.ch>
2013-05-19 13:32:13 +0200

    test

--- Jazz.Inc.Unit.Tests/Class1.cs
+++ Jazz.Inc.Unit.Tests/Class1.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Jazz.Inc.Unit.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Foo()
+        {
+            Assert.IsTrue(true);
+        }
+
     }
 }
